f53950d9d2598c2827cabbfa65b607b27ff10501
Adarsh Kumar <kadars.singh99@gmail.com>
2019-10-18 02:25:36 +0530

    Updates

---
+++ precache-manifest.a355bef2a6d351c72c9b07b9ebdf2ddf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c4319bba405012fc33c66b3e78e7528",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "a977620aa0d4bddf1033",
+    "url": "/chat/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "61cd5417701de5649676",
+    "url": "/chat/static/css/main.2d653c6c.chunk.css"
+  },
+  {
+    "revision": "a977620aa0d4bddf1033",
+    "url": "/chat/static/js/2.eec66d18.chunk.js"
+  },
+  {
+    "revision": "61cd5417701de5649676",
+    "url": "/chat/static/js/main.ea55e198.chunk.js"
+  },
+  {
+    "revision": "298f2e12cf19d5dd99f0",
+    "url": "/chat/static/js/runtime-main.f21441e0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/chat/static/media/logo.25bf045c.svg"
+  }
+]);
